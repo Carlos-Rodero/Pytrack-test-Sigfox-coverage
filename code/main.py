a43443db05607af166a40b98bb9fc890f6038347
Carlos-Rodero <carlos.rodero.garcia@gmail.com>
2019-03-08 10:31:04 +0100

    deep sleep 300 s

--- code/main.py
+++ code/main.py
@@ -25,7 +25,7 @@ deployment_seq = None
 # Time values in seconds of different variables
 time_searching_GPS = 30
 time_searching_Sigfox = 60
-time_to_deep_sleep = 600
+time_to_deep_sleep = 300
 
 # Boolean to save if data have been sent to Sigfox
 data_sent = False
